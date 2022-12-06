6cadd718ac3e2203b977cda1af94a8fccc429f14
pwoogi <chrishw1029@gmail.com>
2022-12-06 21:44:55 +0900

    queryDsl paging 결과 테스트

--- src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -164,4 +164,20 @@ public class QuerydslBasicTest {
 
         assertThat(result.size()).isEqualTo(2);
     }
+
+    @Test
+    public void paging2(){
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(queryResults.getTotal()).isEqualTo(5);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+
+    }
 }
