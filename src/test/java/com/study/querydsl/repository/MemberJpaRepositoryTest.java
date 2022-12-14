583d8c44d6afa18da24dd6181cc230b4813f1c69
pwoogi <chrishw1029@gmail.com>
2022-12-14 19:38:01 +0900

    동적 쿼리 성능 최적화 조회 (복습)

--- src/test/java/com/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.study.querydsl.repository;
 
+import com.study.querydsl.dto.MemberSearchCondition;
+import com.study.querydsl.dto.MemberTeamDto;
 import com.study.querydsl.entity.Member;
+import com.study.querydsl.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -35,6 +38,34 @@ class MemberJpaRepositoryTest {
 
         List<Member> result2 = memberJpaRepository.findByUsername_QueryDsl("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void searchTest(){
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition memberSearchCondition = new MemberSearchCondition();
+        memberSearchCondition.setAgeGoe(35);
+        memberSearchCondition.setAgeLoe(40);
+//        memberSearchCondition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(memberSearchCondition);
+
+        assertThat(result).extracting("username").containsExactly("member4");
+
     }
 
 
