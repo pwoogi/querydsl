99d2028b1ad5e2324bc1dd0228367ae4464d3596
pwoogi <chrishw1029@gmail.com>
2022-12-16 17:26:21 +0900

    페이징처리 복습

--- src/main/java/com/study/querydsl/repository/MemberRepository.java
+++ src/main/java/com/study/querydsl/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.study.querydsl.repository;
 import com.study.querydsl.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+
+    List<Member> findByUsername(String username);
 }
